d41c4ac7f1b78be2b17000fc5dc00b5595755dcc
ThymomenosGata <ThymomenosGata@MacBook-Air-mac.local>
2019-01-29 14:02:47 +0300

    generate repository

---
+++ app/src/main/java/org/wordy/testtask/MainActivity.java
@@ -0,0 +1,13 @@
+package org.wordy.testtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
